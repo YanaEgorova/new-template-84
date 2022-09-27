f0c67f60dd3727ca504445a1131b5b64e3b8f28e
Yana <yanaegorova69.3@gmail.com>
2022-09-28 01:26:53 +0300

    +

--- js/templates/product-item.js
+++ js/templates/product-item.js
@@ -1,26 +1,25 @@
+
+
 export const item = (product) => {
-
   return `
-  <li class="item"> 
-   <div class="product__block js_product glass glass-m" id="${product.id}">
-   <a href="product-page.html?id=${product.id}" class="product__link">
-
-      <div class="product__image-box">
-          <div class="product__image-block">
-              <img src="${product.image}" alt="" class="img">
-          </div>
-          <div class="product__text-box">
-          <p class="product__name js_product-name">${product.name}</p>
-          <span class="product__price">
-          $${(product.price).toFixed(2)}
-          </span>
-          </div>
-      </div>
-      </a>
-      <button class="product__btn js_add-to-cart"></button>
+    <li class="item">
+    <div class="product__block js_product glass glass-m" id="${product.id}">
+    <div class="product__img-box">
+     <img src="${product.image}" alt="" class="img">
     </div>
-  </li>
+    <div class="product__text-box">
+    <p class="product__name js_product-name">${product.name}</p>
+      <span class="product__price">$${(product.price).toFixed(2)}</span>
+    </div>
+    <div class="product__btn-block">
+    <a href="product-page.html?id=${product.id}" class="btn btn--first" style="margin: ${product.isItRing ? 'auto' : ''}">details</a>
+    ${
+      product.isItRing ? '' : '<button class="btn js_add-to-cart">buy</button>'
+    }
+    </div>
+    </div>
+    </li>
     `
-}
+  }
 
 
